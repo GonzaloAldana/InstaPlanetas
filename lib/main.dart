2060fbb3d45f8125ba33fac63219de330a8e6c8c
GonzaloAldana <gon.zalo.music97@gmail.com>
2019-10-19 08:07:10 -0500

    Helper Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,154 @@
-import 'dart:io';
-import 'package:path/path.dart';
-import 'package:animated_card/animated_card.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'card_widget.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+
+        //
+
+        // Try running your application with "flutter run". You'll see the
+
+        // application has a blue toolbar. Then, without quitting the app, try
+
+        // changing the primarySwatch below to Colors.green and then invoke
+
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+
+        // counter didn't reset back to zero; the application is not restarted.
+
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Hello World'),
+    );
+  }
 }
 
-File imagen;
-String filename;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
-  Future _getImage(ImageSource source) async {
-    var selectedImage = await ImagePicker.pickImage(source: source);
+  // This widget is the home page of your application. It is stateful, meaning
 
+  // that it has a State object (defined below) that contains fields that affect
+
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+
+  // case the title) provided by the parent (in this case the App widget) and
+
+  // used by the build method of the State. Fields in a Widget subclass are
+
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
     setState(() {
-      if (selectedImage != null) {
-        imagen = selectedImage;
-        filename = basename(imagen.path);
-      }
+      // This call to setState tells the Flutter framework that something has
+
+      // changed in this State, which causes it to rerun the build method below
+
+      // so that the display can reflect the updated values. If we changed
+
+      // _counter without calling setState(), then the build method would not be
+
+      // called again, and so nothing would appear to happen.
+
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'InstaPlanetas',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("InstaPlanetas"),
-          ),
-          body: Container(
-            child: StreamBuilder(
-              stream: Firestore.instance.collection('planetas').snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) return new Text('Loading...');
-                print("asdasdasd");
-                return ListView(
-                    shrinkWrap: true,
-                    children: snapshot.data.documents.map((document) {
-                      return Column(
-                        children: <Widget>[
-                          AnimatedCard(
-                              child: CardWidget(document['nombre'],
-                                  document['imagen'].cast<String>()))
-                        ],
-                      );
-                    }).toList());
-              },
+    // This method is rerun every time setState is called, for instance as done
+
+    // by the _incrementCounter method above.
+
+    //
+
+    // The Flutter framework has been optimized to make rerunning build methods
+
+    // fast, so that you can just rebuild anything that needs updating rather
+
+    // than having to individually change instances of widgets.
+
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+
+        // the App.build method, and use it to set our appbar title.
+
+        title: new Text(widget.title),
+      ),
+
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+
+        // in the middle of the parent.
+
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+
+          // arranges them vertically. By default, it sizes itself to fit its
+
+          // children horizontally, and tries to be as tall as its parent.
+
+          //
+
+          // Invoke "debug paint" (press "p" in the console where you ran
+
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+
+          // window in IntelliJ) to see the wireframe for each widget.
+
+          //
+
+          // Column has various properties to control how it sizes itself and
+
+          // how it positions its children. Here we use mainAxisAlignment to
+
+          // center the children vertically; the main axis here is the vertical
+
+          // axis because Columns are vertical (the cross axis would be
+
+          // horizontal).
+
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.publish),
-            onPressed: () {
-              _getImage(ImageSource.gallery);
-            },
-          ),
-        ));
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
-}
-
-Future<String> uploadImage(double lat, double lon, String nombre) async {
-  StorageReference ref = FirebaseStorage.instance.ref().child(filename);
-  StorageUploadTask uploadTask = ref.putFile(imagen);
-  var downUrl = await (await uploadTask.onComplete).ref.getDownloadURL();
-  var url = downUrl.toString();
-  List<String> urls = [];
-  urls.add(url);
-
-  print("URL de imagen: $url");
-  Firestore.instance
-      .collection('marcadorNombre')
-      .document()
-      .setData({'lat': lat, 'lon': lon, 'nombre': nombre, 'imagen': urls});
-  imagen = null;
-  return url;
 }
